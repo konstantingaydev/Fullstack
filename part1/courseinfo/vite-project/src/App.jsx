b933c63cc3d4ccae0a4f2e89b194bfc2f400756b
konstantingaydev <konstantingaydev16@gmail.com>
2025-08-08 16:22:26 -0700

    created starter Vite app for courseinfo and completed part1 defining new App components

---
+++ part1/courseinfo/vite-project/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  console.log('Header name is: '+props.name)
+  return (
+    <>
+      <h1>{props.name}</h1>
+    </>
+  )
+  const course = 'Half Stack application development'
+}
+
+const Content = (props) => {
+  console.log('Content name is: '+props.name +' with this many exercises: '+props.count)
+  return (
+    <>
+      <p>
+        {props.name} {props.count}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log('Total amount of exercises is: '+props.count)
+  return (
+    <>
+      <p>
+        Number of exercises {props.count}
+      </p>
+    </>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course}/>
+      <Content name={part1} count={exercises1}/>
+      <Content name={part2} count={exercises2}/>
+      <Content name={part3} count={exercises3}/>
+      <Total count = {exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
